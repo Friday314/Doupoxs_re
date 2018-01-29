4f242e263bbfa08e5e4b4105d5a32abd172db613
Friday314 <zhangzheng@email.com>
2018-01-29 10:45:22 +0800

    Commit

--- zz_00.py
+++ zz_00.py
@@ -18,6 +18,7 @@ def get_info(url):
 
     res = requests.get(url, headers=_headers)
 
+    # 网址请求成功进入
     if res.status_code == 200:
 
         contents = re.findall("<p>(.*?)</p>", res.content.decode("utf-8"), re.S)
@@ -26,6 +27,7 @@ def get_info(url):
 
             print(content)
 
+            # 写入文件
             f.write(content+"\n")
 
     else:
@@ -33,17 +35,20 @@ def get_info(url):
 
 
 if __name__ == "__main__":
+
+    # 遍历网址
     urls = ["http://www.doupoxs.com/doupocangqiong/{}.html".format(str(i))
             for i in range(2, 100)]
 
-    print(urls)
+    # print(urls)
 
     for url in urls:
 
         get_info(url)
 
-        print(url)
+        # print(url)
 
+        # 睡眠一秒
         time.sleep(1)
 
 
